593b8d099aa06adf39d36bd186daacaa7fcdff3d
quackersian <54241767+quackersian@users.noreply.github.com>
2022-02-21 10:26:58 +0000

    v0.1.0

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import logging
 script_version = "0.1.0"
 script_date = "2022-02-18"
-bot_name = "Some Bot Name Here"
-github_link = "Some GitHub Link Here"
-discord_server = "Some Discord Link here"
+bot_name = "Big Spender Assistant"
+github_link = ""
+discord_server = ""
 logging_level = logging.DEBUG
 test_guild_id = 864438904954159104
