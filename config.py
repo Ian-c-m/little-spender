8bd1d248524383df5cb58e342a537eb892fe8536
quackersian <54241767+quackersian@users.noreply.github.com>
2022-02-22 11:56:23 +0000

    v0.1.0

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ script_date = "2022-02-18"
 bot_name = "Big Spender Assistant"
 github_link = ""
 discord_server = ""
-logging_level = logging.DEBUG
+logging_level = logging.info
 test_guild_id = 864438904954159104
--- main.py
+++ main.py
@@ -22,18 +22,71 @@ def setup_logging():
     except Exception as e:
         print(f"ERROR - failed to setup logging - {e}") 
 
+
+
 #Alerts once the bot is ready to receive commands
 @bot.event
 async def on_ready():
     print(f"{config.bot_name} ready")
 
+
+
+
 #An example slash command, will respond World when you use /hello
-@bot.slash_command(description="Responds with 'World'")
+@bot.slash_command(description = "Responds with 'World'")
 async def hello(inter):
 
     await inter.send("World")
     
     
+
+@bot.slash_command(description = "Adds/removes a role.")
+async def roles(inter, role: disnake.Role, remove: bool):
+
+    if remove == True:
+    #the member wants to remove the role
+
+        logging.info(f"{inter.author} tried to remove {role} role.")
+
+        try:
+            await inter.author.remove_roles(role, reason = "Removed via slash command", atomic = False)
+            logging.info(f"Succesfully removed {role} role from {inter.author}.")
+            await inter.send(f"Succesfully removed {role} role.")
+            
+        
+        except disnake.errors.Forbidden as e:
+            #bot didn't have sufficient permissions to remove role.
+            logging.exception(f"Could not remove {role} from {inter.author}, invalid permissions.")
+            await inter.send("This action failed, please try again.")
+        
+        except Exception as e:
+            #generic error
+            logging.exception(f"Could not remove {role} from {inter.author}. {e}")
+            await inter.send("This action failed, please try again.")
+
+
+    elif remove == False:
+    #the member wants to add the role
+
+        logging.info(f"{inter.author} tried to add {role} role.")
+        
+        try:
+            await inter.author.add_roles(role, reason = "Added via slash command", atomic = False)
+            logging.info(f"Succesfully added {role} role from {inter.author}.")
+            await inter.send(f"Succesfully added {role} role.")
+            
+        
+        except disnake.errors.Forbidden as e:
+            #bot didn't have sufficient permissions to add role.
+            logging.exception(f"Could not add {role} from {inter.author}, invalid permissions.")
+            await inter.send("This action failed, please try again.")
+        
+        except Exception as e:
+            #generic error
+            logging.exception(f"Could not add {role} from {inter.author}. {e}.")
+            await inter.send("This action failed, please try again.")
+
+
 
 if __name__ == "__main__":
     setup_logging()
